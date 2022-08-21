99e5e77c2343932153cc6b1b13123e239adc70d2
Daniil Sorokin <99132300+Deadpul378@users.noreply.github.com>
2022-08-21 12:12:38 +0300

    complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+let colorChangeInterval = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+refs.startBtn.addEventListener('click', startColorChange);
+refs.stopBtn.addEventListener('click', stopColorChange);
+
+function startColorChange() {
+  colorChangeInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+}
+
+function stopColorChange() {
+  clearInterval(colorChangeInterval);
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formBlock = document.querySelector('.form');
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  })
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+function onCreatePromises(e) {
+  e.preventDefault();
+  const { delay, step, amount } = e.currentTarget;
+
+  let firstDelay = Number(delay.value);
+  const promisesAmount = Number(amount.value);
+  const delayStep = Number(step.value);
+  for (let i = 1; i <= promisesAmount; i += 1) {
+    createPromise(i, firstDelay);
+    firstDelay += delayStep;
   }
 }
+
+formBlock.addEventListener('submit', onCreatePromises);
